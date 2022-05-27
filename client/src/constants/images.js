26ff890ad6eb369d084992d6af9e27835ff8b618
Antonio Obradović <55183634+AntonioObra@users.noreply.github.com>
2022-05-27 21:17:34 +0200

    added side bar

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -3,7 +3,11 @@ import logo from "../assets/logo.png";
 
 import testAvatar from "../assets/eren.jpeg";
 
+//taken from 'https://mixkit.co/free-stock-art/exhausted-man-in-front-of-a-computer-with-his-head-69/'
+import computer from "../assets/computer.png";
+
 export default {
   logo,
   testAvatar,
+  computer,
 };
--- client/src/container/Blogs/Blogs.jsx
+++ client/src/container/Blogs/Blogs.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import { AppWrap } from "../../wrapper";
 import { BlogCards } from "../../components";
+import { IoIosSearch } from "react-icons/io";
+import { images } from "../../constants";
 import "./Blogs.scss";
 
 const Blogs = () => {
   return (
-    <div className="app__flex app__blogs">
+    <div className=" app__blogs">
       <main className="app__blogs-main">
         <BlogCards />
       </main>
-
-      <aside className="app__blogs-aside">asdf</aside>
+      <div className="vr" />
+      <aside className="app__blogs-aside">
+        <div className="Aside-Search-Div">
+          <button>
+            <IoIosSearch
+              style={{
+                height: "2em",
+                width: "2em",
+                color: "rgba(0, 0, 0, 0.805)",
+              }}
+            />
+          </button>
+          <input type="text" placeholder="Search" />
+        </div>
+        {/* <img src={images.computer} alt="" /> */}
+        <h3>Topics</h3>
+        <div className="Aside-Tags">
+          {["Casual", "Books", "Videogames", "Movies"].map((item, index) => (
+            <div key={index} className={`Aside-Tags-Filter ${item}`}>
+              {item}
+            </div>
+          ))}
+        </div>
+      </aside>
     </div>
   );
 };
